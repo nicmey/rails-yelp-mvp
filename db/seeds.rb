6fcbc025c26fa8c3799d0eb249222f012d50eba5
Nicolas Meyer <myr.nicolas@gmail.com>
2018-08-15 12:42:59 +0200

    first version of yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+5.times do
+  resto = Restaurant.new({
+    name: Faker::Coffee.blend_name,
+    address: "1 Av",
+    phone_number: "0101010",
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  })
+  resto.save!
+end
+
+10.times do
+  review = Review.new({
+    content: ["good", "very good", "not good"].sample,
+    rating: 5
+  })
+  review.restaurant = Restaurant.all.sample
+  review.save!
+end
